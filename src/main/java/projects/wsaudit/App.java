22edf87814c6fef9c52a400e3e3fa2ba644f94c3
aravindwithu <avenkit2@binghamton.edu>
2018-10-04 07:59:48 -0700

    auto increment added.

--- src/main/java/projects/wsaudit/App.java
+++ src/main/java/projects/wsaudit/App.java
@@ -5,6 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import java.sql.Timestamp;
 
 /**
  * Hello world!
@@ -17,8 +18,11 @@ public class App
         System.out.println( "Hello World!" );
         
         TestEntity  e= new TestEntity();
-        e.setKeySet(20);
         e.setValueSet("twenty");
+        e.setDescription("twenty first century");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println(timestamp);
+        e.setT1(timestamp);
        
         Configuration con = new Configuration().configure().addAnnotatedClass(TestEntity.class) ;
         SessionFactory sf = con.buildSessionFactory();
@@ -33,6 +37,5 @@ public class App
         trans = session.beginTransaction();
         session.saveOrUpdate(e);
         trans.commit();
-        
     }
 }
--- src/main/java/projects/wsaudit/TestEntity.java
+++ src/main/java/projects/wsaudit/TestEntity.java
@@ -1,14 +1,23 @@
 package projects.wsaudit;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
+import java.sql.Timestamp;
 
 @Entity
 public class TestEntity 
 {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "keySet", updatable=false, nullable = false)
 	private int keySet;
 	private String valueSet;
+	private String description;
+	private Timestamp t1;
 	
 	public int getKeySet() {
 		return keySet;
@@ -21,5 +30,17 @@ public class TestEntity
 	}
 	public void setValueSet(String valueSet) {
 		this.valueSet = valueSet;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Timestamp getT1() {
+		return t1;
+	}
+	public void setT1(Timestamp t1) {
+		this.t1 = t1;
 	}
 }
